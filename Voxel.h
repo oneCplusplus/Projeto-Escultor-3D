3edf5327310d1f161c836f60255297bc2dc6edce
oneCplusplus <151362720+oneCplusplus@users.noreply.github.com>
2023-11-18 06:08:11 -0300

    Update Voxel.h

--- Voxel.h
+++ Voxel.h
@@ -1,16 +1,10 @@
 #ifndef VOXEL_H
 #define VOXEL_H
 
-/**
- * @brief A estrutura Voxel representa um elemento volumétrico em um espaço tridimensional.
- *
- * Cada voxel possui propriedades como cor (componentes RGB), transparência (alfa), 
- * e uma flag indicando se deve ser mostrado ou não.
- */
 typedef struct {
-  float r, g, b; ///< Componentes de cor: vermelho, verde e azul.
-  float a; ///< Valor de transparência do voxel.
-  bool show; ///< Indica se o voxel deve ser mostrado ou não.
+  float r, g, b;
+  float a;
+  bool show;
 } Voxel;
 
-#endif //VOXEL_H
+#endif
